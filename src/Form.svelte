e6c4a87f2efc47da7052b1ebcffab9a2b856fa46
Nicolas <ndonosofs@gmail.com>
2020-06-28 13:06:22 -0400

    Form-ready

--- src/Form.svelte
+++ src/Form.svelte
@@ -1,6 +1,51 @@
+<script>
+export let id;
+export let name = "";
+export let price;
+
+$: mode = id ? "edit" : "add";
+
+$: canSubmit = price >= 0 && name !== "";
+
+function submit(){
+    if(!canSubmit){
+        return
+    }
+    price = '';
+    name = '';
+    id = undefined;
+}
+
+function cancel(){
+    price = '';
+    name = '';
+    id = undefined;
+}
+
+</script>
+
+<form on:submit|preventDefault={submit}>
+ <fieldset>
+  <label for="nameField
+  ">Material</label>
+  <input type="text" id="nameField" bind:value={name} placeholder="Wood, Glue, Etc.">
+
+   <label for="priceField
+  ">Price</label>
+  <input type="number" min="0" step="any" bind:value={price} id="priceField" placeholder="Price">
+ </fieldset>
+ <button disabled={!canSubmit} class="float-right" type="submit">{mode}</button>
+ {#if mode === 'edit'}
+ <button on:click={cancel} class="float-right" type="button">Cancel</button>
+ {/if}
+</form>
 
 
-
-
-
-<h1>Form</h1>
+<style>
+button{
+    margin-left:20px;
+}
+button:disabled{
+    cursor: not-allowed;
+}
+</style>
